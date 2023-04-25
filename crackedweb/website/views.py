035717bb2b38063fefae48bf8901a6fbd0c14028
Akshatchourey <achourey09@gmail.com>
2023-04-25 09:33:28 +0530

    Making repo lighter by deleting video files.

--- crackedweb/website/views.py
+++ crackedweb/website/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-import webbrowser
-import google
+# import webbrowser, import google
+from googlesearch import  search
 
 def index(request):
-    akshat={"search":webbrowser.open("How to increase concentration")}
+    akshat={"search":search("How to increase concentration", tld="co.in", num=30, stop=30, pause=2)}
     return render(request, "website/index.html",akshat)
 
 
